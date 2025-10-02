e7b2ca4dd6686a3be316f2b12949fb67df86630a
cherry23-sai <kapperacharan55@gmail.com>
2025-10-02 20:38:12 +0530

    feat: configure for deployment

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../models/contact'); 
+const Contact = require('../models/Contact'); 
 const nodemailer = require('nodemailer');
 const twilio = require('twilio'); // Import the Twilio library
 
